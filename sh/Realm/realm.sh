ee7c51f2b5d7a7a54055a9854a1a46c675a3066f
limy90 <79237112+limy90@users.noreply.github.com>
2025-02-20 17:05:39 +0800

    Update realm.sh

--- sh/Realm/realm.sh
+++ sh/Realm/realm.sh
@@ -141,8 +141,8 @@ confirm(){
 }
 
 install_realm(){
-	echo "安装realm 2.6.3版本..."
- 	download /tmp/realm-x86_64-unknown-linux-gnu.tar.gz https://github.com/zhboner/realm/releases/download/v2.6.3/realm-x86_64-unknown-linux-gnu.tar.gz
+	echo "安装realm 2.7.0版本..."
+ 	download /tmp/realm-x86_64-unknown-linux-gnu.tar.gz https://github.com/zhboner/realm/releases/download/v2.7.0/realm-x86_64-unknown-linux-gnu.tar.gz
  	tar -zxvf /tmp/realm-x86_64-unknown-linux-gnu.tar.gz -C /usr/bin/ > /dev/null 2>&1
   	[ ! -f /usr/bin/realm ] && echo -e "[${red}Error${plain}] realm可执行文件不存在" && exit 1
         chown root:root /usr/bin/realm && chmod +x /usr/bin/realm
@@ -152,7 +152,7 @@ install_realm(){
 	[ ! -d /usr/local/etc/realm/ ] && mkdir /usr/local/etc/realm/
 	download  /usr/local/etc/realm/config.toml https://raw.githubusercontent.com/zhouh047/realm-oneclick-install/main/config.toml
 	
-	echo "realm 2.6.3安装成功..."
+	echo "realm 2.7.0安装成功..."
 	[ -f /usr/bin/realm ] && echo -e "${green}installed${plain}: /usr/bin/realm"
 	[ -f /etc/systemd/system/realm.service ] && echo -e "${green}installed${plain}: /etc/systemd/system/realm.service"
 	[ -f /usr/local/etc/realm/config.toml ] && echo  -e "${green}installed${plain}: /usr/local/etc/realm/config.toml"
